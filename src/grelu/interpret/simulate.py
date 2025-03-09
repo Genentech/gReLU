988ad392cdd9a8b7a9c3aec24e0f46598b04a100
avantikalal <avantikalal1990@gmail.com>
2025-03-09 19:14:03 +0000

    add simulate module

---
+++ src/grelu/interpret/simulate.py
@@ -0,0 +1,88 @@
+from typing import Callable, List, Optional, Sequence, Tuple, Union
+
+import numpy as np
+import pandas as pd
+
+from grelu.utils import get_compare_func, get_aggfunc
+
+
+def marginalize_patterns(
+    model: Callable,
+    patterns: Union[str, List[str]],
+    seqs: Union[pd.DataFrame, List[str], np.ndarray],
+    genome: Optional[str] = None,
+    devices: Union[str, int, List[int]] = "cpu",
+    num_workers: int = 1,
+    batch_size: int = 64,
+    n_shuffles: int = 0,
+    seed: Optional[int] = None,
+    prediction_transform: Optional[Callable] = None,
+    rc: bool = False,
+    augment_aggfunc: Optional[Union[str, Callable]] = None,
+    compare_func: Optional[Union[str, Callable]] = None,
+) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+    """
+    Runs a marginalization experiment.
+
+        Given a model, a pattern (short sequence) to insert, and a set of background
+        sequences, get the predictions from the model before and after
+        inserting the patterns into the dinucleotide-shuffled background sequences.
+
+    Args:
+        model: trained model
+        patterns: a sequence or list of sequences to insert
+        seqs: background sequences
+        genome: Name of the genome to use if genomic intervals are supplied
+        device: Index of device on which to run inference
+        num_workers: Number of workers for inference
+        batch_size: Batch size for inference
+        seed: Random seed
+        prediction_transform: A module to transform the model output
+        rc: If True, augment by reverse complementation
+        augment_aggfunc: Function to aggregate the predictions over augmented
+            versions of the same sequence.
+        compare_func: Function to compare the predictions with and without the 
+            pattern. Options are "divide" or "subtract". If not provided, the 
+            predictions before and after pattern insertion will be returned.
+
+    Returns:
+        preds_before: The predictions from the background sequences
+        preds_after: The predictions after inserting the pattern into
+            the background sequences.
+    """
+    # Create torch dataset
+    from grelu.data.dataset import PatternMarginalizeDataset
+
+    # Set transform
+    model.add_transform(prediction_transform)
+
+    # Make marginalization dataset
+    ds = PatternMarginalizeDataset(
+        seqs=seqs,
+        patterns=patterns,
+        genome=genome,
+        rc=rc,
+        n_shuffles=n_shuffles,
+        seed=seed,
+    )
+
+    # Get predictions on the sequences before motif insertion
+    preds = model.predict_on_dataset(
+        ds,
+        devices=devices,
+        num_workers=num_workers,
+        batch_size=batch_size,
+        augment_aggfunc=augment_aggfunc,
+    )  # Output shape: B, shufxn_augmented, motifs+1, T, L
+
+    # Drop transform
+    model.reset_transform()
+
+    # Extract the reference sequence predictions
+    before_preds, after_preds = preds[..., [0], :, :], preds[..., 1:, :, :]
+
+    # Compare sequences with motifs to sequence without any motif
+    if compare_func is None:
+        return before_preds, after_preds
+    else:
+        return get_compare_func(compare_func)(after_preds, before_preds)
