4867b4cfd3f88b06cac4ca659a63921379250826
avantikalal <avantikalal1990@gmail.com>
2025-03-14 04:40:51 +0000

    split modisco functions

--- src/grelu/interpret/modisco.py
+++ src/grelu/interpret/modisco.py
@@ -9,6 +9,61 @@ from typing import Callable, List, Optional, Union
 
 import numpy as np
 import pandas as pd
+from torch import tensor
+
+
+def _ism_attrs(
+    model,
+    seqs: List[str],
+    one_hot: tensor,
+    prediction_transform: Optional[Callable],
+    start: int,
+    end: int,
+    devices: Union[str, int],
+    num_workers: int,
+    batch_size: int,
+    genome: str,
+):
+    """
+    Perform ISM and format the results for TF-Modisco.
+    """
+    from grelu.data.dataset import ISMDataset, SeqDataset
+
+    ref_ds = SeqDataset(seqs, genome=genome)
+    ism_ds = ISMDataset(seqs, drop_ref=True, positions=range(start, end), genome=genome)
+
+    # Add transform to model
+    model.add_transform(prediction_transform)
+
+    # Get predictions for reference sequences
+    ref_preds = model.predict_on_dataset(
+        ref_ds, devices=devices, num_workers=num_workers, batch_size=batch_size
+    )  # B, 1, T, L
+    assert (ref_preds.shape[-1] == 1) and (ref_preds.shape[-2] == 1)
+
+    # Get predictions for all mutated sequences
+    ism_preds = model.predict_on_dataset(
+        ism_ds,
+        devices=devices,
+        num_workers=num_workers,
+        batch_size=batch_size,
+    )  # B, l, 3, 1, 1
+    assert (ism_preds.shape[-1] == 1) and (ism_preds.shape[-2] == 1)
+    ism_preds = ism_preds.squeeze((-1, -2))  # B, l, 3
+
+    # Remove transform
+    model.reset_transform()
+
+    # Get the negative log ratio
+    attrs = -np.log2(np.divide(ism_preds, ref_preds))  # B, l, 3
+
+    # Mean over all possible mutations
+    attrs = np.expand_dims(attrs.mean(-1), 1)  # B, 1, l
+
+    # Multiply by original sequence
+    attrs = np.multiply(attrs, one_hot[:, :, start:end].numpy())  # B, 4, l
+
+    return attrs
 
 
 def _add_tomtom_to_modisco_report(
@@ -105,6 +160,31 @@ def _add_tomtom_to_modisco_report(
         )
 
 
+def _tomtom_on_modisco(
+    out_dir: str,
+    h5_file: str,
+    meme_file: str,
+    top_n_matches: int = 10,
+    trim_threshold: float = 0.3,
+):
+    """
+    Run tomtom on motifs in a modisco report
+    """
+    from grelu.interpret.motifs import run_tomtom
+    from grelu.io.motifs import read_modisco_report
+
+    tomtom_file = os.path.join(out_dir, "tomtom.csv")
+    motifs = read_modisco_report(h5_file, trim_threshold=trim_threshold)
+    tomtom_results = run_tomtom(motifs, meme_file)
+    tomtom_results.to_csv(tomtom_file)
+    _add_tomtom_to_modisco_report(
+        modisco_dir=out_dir,
+        tomtom_results=tomtom_results,
+        meme_file=meme_file,
+        top_n_matches=top_n_matches,
+    )
+
+
 def run_modisco(
     model,
     seqs: Union[pd.DataFrame, np.array, List[str]],
@@ -151,10 +231,7 @@ def run_modisco(
     from modiscolite.report import create_modisco_logos, report_motifs
     from modiscolite.tfmodisco import TFMoDISco
 
-    from grelu.data.dataset import ISMDataset, SeqDataset
-    from grelu.interpret.motifs import run_tomtom
     from grelu.interpret.score import get_attributions
-    from grelu.io.motifs import read_modisco_report
     from grelu.sequence.format import convert_input_type
     from grelu.sequence.utils import get_unique_length
 
@@ -189,42 +266,18 @@ def run_modisco(
 
     elif method == "ism":
         print("Performing ISM")
-        ref_ds = SeqDataset(seqs, genome=genome)
-        ism_ds = ISMDataset(
-            seqs, drop_ref=True, positions=range(start, end), genome=genome
-        )
-
-        # Add transform to model
-        model.add_transform(prediction_transform)
-
-        # Get predictions for reference sequences
-        ref_preds = model.predict_on_dataset(
-            ref_ds, devices=devices, num_workers=num_workers, batch_size=batch_size
-        )  # B, 1, T, L
-        assert (ref_preds.shape[-1] == 1) and (ref_preds.shape[-2] == 1)
-
-        # Get predictions for all mutated sequences
-        ism_preds = model.predict_on_dataset(
-            ism_ds,
+        attrs = _ism_attrs(
+            model=model,
+            seqs=seqs,
+            one_hot=one_hot,
+            prediction_transform=prediction_transform,
+            start=start,
+            end=end,
             devices=devices,
             num_workers=num_workers,
             batch_size=batch_size,
-        )  # B, l, 3, 1, 1
-        assert (ism_preds.shape[-1] == 1) and (ism_preds.shape[-2] == 1)
-        ism_preds = ism_preds.squeeze((-1, -2))  # B, l, 3
-
-        # Remove transform
-        model.reset_transform()
-
-        # Get the negative log ratio
-        attrs = -np.log2(np.divide(ism_preds, ref_preds))  # B, l, 3
-
-        # Mean over all possible mutations
-        attrs = np.expand_dims(attrs.mean(-1), 1)  # B, 1, l
-
-        # Multiply by original sequence
-        attrs = np.multiply(attrs, one_hot_arr)  # B, 4, l
-
+            genome=genome,
+        )
     else:
         raise NotImplementedError
 
@@ -237,43 +290,50 @@ def run_modisco(
         **kwargs,
     )
 
-    print("Writing modisco output")
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    # Check if any patterns were found
+    if pos_patterns is None:
+        n_pos = 0
+    else:
+        n_pos = len(pos_patterns)
 
-    h5_file = os.path.join(out_dir, "modisco_report.h5")
-    save_hdf5(h5_file, pos_patterns, neg_patterns, window_size=20)
+    if neg_patterns is None:
+        n_neg = 0
+    else:
+        n_neg = len(neg_patterns)
 
-    print("Creating sequence logos")
-    modisco_logo_dir = os.path.join(out_dir, "trimmed_logos")
-    if not os.path.isdir(modisco_logo_dir):
-        os.mkdir(modisco_logo_dir)
-    create_modisco_logos(
-        h5_file,
-        modisco_logo_dir,
-        trim_threshold=0.2,
-        pattern_groups=["pos_patterns", "neg_patterns"],
-    )
+    if (n_pos > 0) or (n_neg > 0):
 
-    print("Creating html report")
-    report_motifs(
-        modisco_h5py=h5_file,
-        output_dir=out_dir,
-        img_path_suffix=out_dir,
-        meme_motif_db=None,
-        is_writing_tomtom_matrix=False,
-    )
+        print(f"{n_pos} positive and {n_neg} negative patterns were found.")
+        print("Writing modisco output")
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
 
-    if meme_file is not None:
-        print("Running TOMTOM")
-        tomtom_file = os.path.join(out_dir, "tomtom.csv")
-        motifs = read_modisco_report(h5_file, trim_threshold=0.3)
-        if len(motifs) > 0:
-            tomtom_results = run_tomtom(motifs, meme_file)
-            tomtom_results.to_csv(tomtom_file)
-            _add_tomtom_to_modisco_report(
-                modisco_dir=out_dir,
-                tomtom_results=tomtom_results,
-                meme_file=meme_file,
-                top_n_matches=10,
-            )
+        h5_file = os.path.join(out_dir, "modisco_report.h5")
+        save_hdf5(h5_file, pos_patterns, neg_patterns, window_size=20)
+
+        print("Creating sequence logos")
+        modisco_logo_dir = os.path.join(out_dir, "trimmed_logos")
+        if not os.path.isdir(modisco_logo_dir):
+            os.mkdir(modisco_logo_dir)
+        create_modisco_logos(
+            h5_file,
+            modisco_logo_dir,
+            trim_threshold=0.2,
+            pattern_groups=["pos_patterns", "neg_patterns"],
+        )
+
+        print("Creating html report")
+        report_motifs(
+            modisco_h5py=h5_file,
+            output_dir=out_dir,
+            img_path_suffix=out_dir,
+            meme_motif_db=None,
+            is_writing_tomtom_matrix=False,
+        )
+
+        if meme_file is not None:
+            print("Running TOMTOM")
+            _tomtom_on_modisco(out_dir=out_dir, h5_file=h5_file, meme_file=meme_file)
+
+    else:
+        print("No patterns were found.")
