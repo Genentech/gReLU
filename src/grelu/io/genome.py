f84246e091cd3070c091ff5ef44343afc976b709
Muhammed Hasan Celik <celik.muhammed_hasan@gene.com>
2025-06-23 18:24:17 -0700

    custom fasta

--- src/grelu/io/genome.py
+++ src/grelu/io/genome.py
@@ -6,8 +6,49 @@ gReLU depends upon genomepy for many of these utilities. See https://vanheeringe
 import os
 from typing import List, Optional, Union
 
-import genomepy
 import pandas as pd
+import pyfaidx
+
+
+class Genome:
+    def __init__(self, genome: str, **kwargs):
+        self.genome = genome
+        self.custom_genome = os.path.isfile(genome)
+        if not self.custom_genome:
+            import genomepy
+            if genome not in genomepy.list_installed_genomes():
+                self._genome = genomepy.install_genome(genome, annotation=False, **kwargs)
+            else:
+                self._genome = genomepy.Genome(genome)
+        else:
+            self._genome = pyfaidx.Fasta(genome, rebuild=False)
+            fai_file = genome + ".fai"
+            if not os.path.isfile(fai_file):
+                raise FileNotFoundError(
+                    f"Genome file {fai_file} not found. "
+                    "Please provide a genome name or a path to a chromosome sizes file. "
+                    f"Or generate one with: `samtools faidx {genome}`."
+                )
+            self._sizes_file = genome + ".sizes"
+
+    def get_seq(self, chrom: str, start: int, end: int, rc: bool = False) -> str:
+        """
+        Get the sequence for a given chromosome and interval.
+        """
+        return self._genome.get_seq(chrom, start, end, rc=rc)
+        
+    @property
+    def sizes_file(self) -> str:
+        if self.custom_genome:
+            if not os.path.isfile(self._sizes_file):
+                raise FileNotFoundError(
+                    f"Genome file {self._sizes_file} not found. "
+                    "Please provide a genome name or a path to a chromosome sizes file. "
+                    f"Or generate one with: `faidx -i chromsizes {self.genome} > {self._sizes_file}`."
+                )
+            return self._sizes_file
+        else:
+            return self._genome.sizes_file
 
 
 def read_sizes(genome: str = "hg38") -> pd.DataFrame:
@@ -24,16 +65,13 @@ def read_sizes(genome: str = "hg38") -> pd.DataFrame:
         and "size" (chromosome size).
     """
     # Get file path
-    if not os.path.isfile(genome):
-        genome = get_genome(genome).sizes_file
-
-    # Read file
+    genome = get_genome(genome).sizes_file
     return pd.read_table(
         genome, header=None, names=["chrom", "size"], dtype={"chrom": str, "size": int}
     )
 
 
-def get_genome(genome: str, **kwargs) -> genomepy.Genome:
+def get_genome(genome: str, **kwargs):
     """
     Install a genome from genomepy and load it as a Genome object
 
@@ -44,11 +82,7 @@ def get_genome(genome: str, **kwargs) -> genomepy.Genome:
     Returns:
         Genome object
     """
-    if genome not in genomepy.list_installed_genomes():
-        return genomepy.install_genome(genome, annotation=False, **kwargs)
-    else:
-        return genomepy.Genome(genome)
-
+    return Genome(genome, **kwargs)
 
 def read_gtf(
     genome: str, features: Optional[Union[str, List[str]]] = None
@@ -68,6 +102,7 @@ def read_gtf(
         GTF annotations
     """
     from grelu.utils import make_list
+    import genomepy
 
     # Read GTF annotations
     try:
